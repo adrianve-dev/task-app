d6c9ed82bb8699bc32ab81abfa957da3a7d9c3d3
adrianve <adrianve@pm.me>
2022-04-06 15:46:31 -0500

    refactor: ensure custom place is readable
    uppercase first letter and lowercase rest of string

--- src/utils/placeUtils.ts
+++ src/utils/placeUtils.ts
@@ -3,5 +3,5 @@ import { Place } from "../Types/place";
 export const placeToString = (place: Place): string => {
     if(place === 'home') return '🏡 Home'
     else if(place === 'work') return '💼 Work'
-    else return `📍 ${place.custom}`
+    else return `📍 ${place.custom.slice(0,1).toUpperCase()}${place.custom.slice(1).toLowerCase()}`
 }
